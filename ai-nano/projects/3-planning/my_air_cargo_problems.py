b7ae729cb66641c8a55935d443867c1cf97fca62
tommytracey <tommytracey@gmail.com>
2017-07-25 19:55:21 +0200

    fixed bug impacting p2 results

--- ai-nano/projects/3-planning/my_air_cargo_problems.py
+++ ai-nano/projects/3-planning/my_air_cargo_problems.py
@@ -262,7 +262,6 @@ def air_cargo_p1() -> AirCargoProblem:
             ]
     return AirCargoProblem(cargos, planes, airports, init, goal)
 
-
 def air_cargo_p2() -> AirCargoProblem:
     ''' Problem 2 Definition:
     Init(At(C1, SFO) ∧ At(C2, JFK) ∧ At(C3, ATL)
@@ -286,33 +285,38 @@ def air_cargo_p2() -> AirCargoProblem:
             expr('At(P3, ATL)'),
            ]
     neg = [
+            expr('At(C1, ATL)'),
+            expr('At(C1, JFK)'),
+            expr('In(C1, P1)'),
+            expr('In(C1, P2)'),
+            expr('In(C1, P3)'),
+            #
+            expr('At(C2, ATL)'),
+            expr('At(C2, SFO)'),
+            expr('In(C2, P1)'),
+            expr('In(C2, P2)'),
+            expr('In(C2, P3)'),
+            #
             expr('At(C3, JFK)'),
             expr('At(C3, SFO)'),
             expr('In(C3, P1)'),
             expr('In(C3, P2)'),
             expr('In(C3, P3)'),
-            expr('At(C2, SFO)'),
-            expr('At(C2, ATL)'),
-            expr('In(C2, P1)'),
-            expr('In(C2, P2)'),
-            expr('In(C2, P3)'),
-            expr('At(C1, JFK)'),
-            expr('At(C1, ATL)'),
-            expr('In(C1, P1)'),
-            expr('In(C1, P2)'),
-            expr('In(C1, P3)'),
+            #
+            expr('At(P1, ATL)'),
             expr('At(P1, JFK)'),
+            expr('At(P2, ATL)'),
             expr('At(P2, SFO)'),
-            expr('At(P3, ATL)'),
+            expr('At(P3, JFK)'),
+            expr('At(P3, SFO)'),
            ]
     init = FluentState(pos, neg)
     goal = [
             expr('At(C1, JFK)'),
             expr('At(C2, SFO)'),
-            expr('At(C3, ATL)'),
+            expr('At(C3, SFO)'),
             ]
     return AirCargoProblem(cargos, planes, airports, init, goal)
-
 
 def air_cargo_p3() -> AirCargoProblem:
     ''' Problem 3 Definition:
@@ -340,29 +344,34 @@ def air_cargo_p3() -> AirCargoProblem:
             expr('At(C1, ATL)'),
             expr('At(C1, JFK)'),
             expr('At(C1, ORD)'),
+            expr('In(C1, P1)'),
+            expr('In(C1, P2)'),
+            #
             expr('At(C2, ATL)'),
             expr('At(C2, ORD)'),
             expr('At(C2, SFO)'),
+            expr('In(C2, P1)'),
+            expr('In(C2, P2)'),
+            #
             expr('At(C3, JFK)'),
             expr('At(C3, ORD)'),
             expr('At(C3, SFO)'),
+            expr('In(C3, P1)'),
+            expr('In(C3, P2)'),
+            #
             expr('At(C4, ATL)'),
             expr('At(C4, JFK)'),
             expr('At(C4, SFO)'),
+            expr('In(C4, P1)'),
+            expr('In(C4, P2)'),
+            #
             expr('At(P1, ATL)'),
             expr('At(P1, JFK)'),
             expr('At(P1, ORD)'),
+            #
             expr('At(P2, ATL)'),
             expr('At(P2, ORD)'),
             expr('At(P2, SFO)'),
-            expr('In(C1, P1)'),
-            expr('In(C1, P2)'),
-            expr('In(C2, P1)'),
-            expr('In(C2, P2)'),
-            expr('In(C3, P1)'),
-            expr('In(C3, P2)'),
-            expr('In(C4, P1)'),
-            expr('In(C4, P2)'),
            ]
     init = FluentState(pos, neg)
     goal = [
