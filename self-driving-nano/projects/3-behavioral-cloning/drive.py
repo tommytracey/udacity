269b50921bea9e4b27bd096facc73755cb183113
tommytracey <tommytracey@gmail.com>
2017-08-22 15:29:32 +0200

    added resizing

--- self-driving-nano/projects/3-behavioral-cloning/drive.py
+++ self-driving-nano/projects/3-behavioral-cloning/drive.py
@@ -44,7 +44,7 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 15
+set_speed = 30
 controller.set_desired(set_speed)
 
 
@@ -61,6 +61,8 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        # Customization: Crop & Resize
+        image_array = cv2.resize(image_array[60:140,20:300],(64,64))
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
