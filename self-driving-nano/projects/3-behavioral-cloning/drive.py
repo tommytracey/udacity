7567ce661962849ca1d1e0ae48ffd4408a0c3b6b
tommytracey <tommytracey@gmail.com>
2017-09-04 00:04:51 +0200

    repo cleanup

--- self-driving-nano/projects/3-behavioral-cloning/drive.py
+++ self-driving-nano/projects/3-behavioral-cloning/drive.py
@@ -63,7 +63,7 @@ def telemetry(sid, data):
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
         # Customization: Crop & Resize
-        image_array = cv2.resize(image_array[60:140,20:300],(64,64))
+        image_array = cv2.resize(image_array[70:140,0:320],(128,64))
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
